4933bfa1e7e4f4cb1740c4db5538dcb4d9506035
Pavan <101943293+Pavana004@users.noreply.github.com>
2023-01-11 14:53:37 +0530

    code

--- src/container/Search.tsx
+++ src/container/Search.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = () => {
   return (
     <div className="cal">
-      <input className='from-control' placeholder='search'/>
+      search
     </div>
   )
 }
